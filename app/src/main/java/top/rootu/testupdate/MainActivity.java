02536a77443fcf46f37ddd39eb789f5050bf85db
usmanec <root@usmancity.ru>
2022-05-03 12:38:39 +0300

    migrate to androidx

--- app/src/main/java/top/rootu/testupdate/MainActivity.java
+++ app/src/main/java/top/rootu/testupdate/MainActivity.java
@@ -1,6 +1,6 @@
 package top.rootu.testupdate;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
