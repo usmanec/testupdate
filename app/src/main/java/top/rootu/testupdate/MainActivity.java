d5b05bf7d52b4d8becb6ef70ae82bfa2536fa479
usmanec <root@usmancity.ru>
2022-05-03 12:47:42 +0300

    lintOptions abortOnError false

--- app/src/main/java/top/rootu/testupdate/MainActivity.java
+++ app/src/main/java/top/rootu/testupdate/MainActivity.java
@@ -1,7 +1,8 @@
 package top.rootu.testupdate;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
